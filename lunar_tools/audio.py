01f0331f4d8644f371fd176b2d60b0413bb60fe0
Johannes Stelzer <jsdmail@gmail.com>
2023-11-20 18:02:15 +0100

    audio recorder upd, added logprint

--- lunar_tools/audio.py
+++ lunar_tools/audio.py
@@ -2,15 +2,48 @@
 # -*- coding: utf-8 -*-
 
 import pyaudio
+import tempfile
 import wave
 from pydub import AudioSegment
 import threading
+import os
+from openai import OpenAI
+from logprint import Logger
 
 
 class AudioRecorder:
-    def __init__(self, output_filename, format=pyaudio.paInt16, channels=1, rate=44100, chunk=1024):
-        self.output_filename = output_filename
-        self.format = format
+    """
+    A class to handle audio recording.
+
+    Attributes:
+        audio_format (pyaudio.paInt16): Format of the audio recording.
+        channels (int): Number of audio channels.
+        rate (int): Sampling rate.
+        chunk (int): Number of frames per buffer.
+        frames (list): List to hold audio frames.
+        is_recording (bool): Flag to check if recording is in progress.
+        audio (pyaudio.PyAudio): PyAudio instance.
+        stream (pyaudio.Stream): Audio stream.
+        output_filename (str): Output file name.
+    """
+
+    def __init__(
+        self,
+        audio_format=pyaudio.paInt16,
+        channels=1,
+        rate=44100,
+        chunk=1024
+    ):
+        """
+        Initialize the audio recorder.
+
+        Args:
+            audio_format (constant): Format of the audio recording.
+            channels (int): Number of audio channels.
+            rate (int): Sampling rate.
+            chunk (int): Number of frames per buffer.
+        """
+        self.audio_format = audio_format
         self.channels = channels
         self.rate = rate
         self.chunk = chunk
@@ -18,9 +51,20 @@ class AudioRecorder:
         self.is_recording = False
         self.audio = pyaudio.PyAudio()
         self.stream = None
+        self.output_filename = None
 
     def _record(self):
-        self.stream = self.audio.open(format=self.format, channels=self.channels, rate=self.rate, input=True, frames_per_buffer=self.chunk)
+        """
+        Internal method to handle the audio recording process.
+        Converts the recorded frames to MP3 format.
+        """
+        self.stream = self.audio.open(
+            format=self.audio_format,
+            channels=self.channels,
+            rate=self.rate,
+            input=True,
+            frames_per_buffer=self.chunk
+        )
         print("Recording...")
         self.frames = []
 
@@ -33,33 +77,47 @@ class AudioRecorder:
         self.stream.close()
         self.audio.terminate()
 
-        # Save the recorded data as a WAV file
-        wf = wave.open(self.output_filename + ".wav", 'wb')
-        wf.setnchannels(self.channels)
-        wf.setsampwidth(self.audio.get_sample_size(self.format))
-        wf.setframerate(self.rate)
-        wf.writeframes(b''.join(self.frames))
-        wf.close()
-
         # Convert to MP3
-        wav_audio = AudioSegment.from_wav(self.output_filename + ".wav")
-        mp3_filename = self.output_filename + ".mp3"
-        wav_audio.export(mp3_filename, format="mp3")
+        raw_data = b''.join(self.frames)
+        wav_audio = AudioSegment(
+            data=raw_data,
+            sample_width=self.audio.get_sample_size(self.audio_format),
+            frame_rate=self.rate,
+            channels=self.channels
+        )
+        wav_audio.export(self.output_filename, format="mp3")
 
-    def start_recording(self):
+    def start_recording(self, output_filename=None):
+        """
+        Start the audio recording.
+
+        Args:
+            output_filename (str): The filename for the output file. If None, a temporary file is created.
+        """
         if not self.is_recording:
             self.is_recording = True
+            if output_filename is None:
+                temp_file = tempfile.NamedTemporaryFile(suffix='.mp3', delete=False)
+                self.output_filename = temp_file.name
+                temp_file.close()
+            else:
+                if not output_filename.endswith('.mp3'):
+                    raise ValueError("Output filename must have a .mp3 extension")
+                self.output_filename = output_filename
             self.thread = threading.Thread(target=self._record)
             self.thread.start()
 
     def stop_recording(self):
+        """
+        Stop the audio recording and join the recording thread.
+        """
         if self.is_recording:
             self.is_recording = False
             self.thread.join()
 
 
-class Whisper:
-    def __init__(self, client=None, logger=None):
+class SpeechDetector:
+    def __init__(self, client=None, logger=None, init_audiorecorder=False):
         if client is None:
             api_key = os.environ.get("OPENAI_API_KEY")
             if not api_key:
@@ -68,3 +126,41 @@ class Whisper:
         else:
             self.client = client
         self.logger = logger if logger else Logger()
+        if init_audiorecorder:
+            self.audio_recorder = AudioRecorder()
+        else:
+            self.audio_recorder = None
+
+    def start_recording(self):
+        if self.audio_recorder is None:
+            raise ValueError("Audio recorder is not available")
+        self.audio_recorder.start_recording()
+
+    def stop_recording(self):
+        if self.audio_recorder is None:
+            raise ValueError("Audio recorder is not available")
+        self.audio_recorder.stop_recording()
+        return self.translate(self.audio_recorder.output_filename)
+
+    def translate(self, audio_filepath):
+        if not os.path.exists(audio_filepath):
+            raise FileNotFoundError(f"Audio file not found: {audio_filepath}")
+        with open(audio_filepath, "rb") as audio_file:
+            transcript = self.client.audio.translations.create(
+                model="whisper-1", 
+                file=audio_file
+            )
+        return transcript.text
+
+    
+
+#%% EXAMPLE USE        
+if __name__ == "__main__":
+    
+    import time
+    speech_detector = SpeechDetector(init_audiorecorder=True)
+    speech_detector.start_recording()
+    time.sleep(3)
+    speech_detector.stop_recording()
+    
+    
---
+++ lunar_tools/logprint.py
@@ -0,0 +1,51 @@
+import os
+import logging
+from datetime import datetime
+from rich.console import Console
+from rich.logging import RichHandler
+
+class Logger:
+    def __init__(self, filename=None):
+        # Determine the filename based on current date and time if not provided
+        if filename is None:
+            now = datetime.now()
+            filename = f"logs/{now.strftime('%y%m%d_%H%M')}.txt"
+            os.makedirs(os.path.dirname(filename), exist_ok=True)
+
+        self.console = Console()
+        self.logger = logging.getLogger(__name__)
+        self.logger.setLevel(logging.INFO)
+
+        # Rich console handler for colorful output
+        rich_handler = RichHandler(rich_tracebacks=True, console=self.console)
+        rich_handler.setFormatter(logging.Formatter('%(message)s'))
+        self.logger.addHandler(rich_handler)
+
+        # File handler without color formatting
+        file_handler = logging.FileHandler(filename, mode='w')
+        file_handler.setFormatter(logging.Formatter('%(asctime)s - %(levelname)s - %(message)s'))
+        self.logger.addHandler(file_handler)
+
+    def print(self, message, color=None):
+        # Printing with color using Rich
+        if color:
+            self.console.print(message, style=f"bold {color}")
+        else:
+            self.console.print(message)
+        
+        # Temporarily disable console handlers, log the message, then re-enable
+        console_handlers = [h for h in self.logger.handlers if isinstance(h, RichHandler)]
+        for h in console_handlers:
+            self.logger.removeHandler(h)
+
+        self.logger.info(message)
+
+        for h in console_handlers:
+            self.logger.addHandler(h)
+
+if __name__ == "__main__":
+    # Example usage
+    logger = Logger()  # No filename provided, will use default
+    logger.print("Hello, world!", "green")
+    logger.print("Warning message", "yellow")
+    logger.print("Error message", "red")
