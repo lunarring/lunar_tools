7e4b9fa49b5edc002e8aab4805ed3e0232d62dd8
Johannes Stelzer <jsdmail@gmail.com>
2023-11-20 20:22:30 +0100

    version

--- setup.py
+++ setup.py
@@ -2,7 +2,7 @@ from setuptools import setup, find_packages
 
 setup(
     name='lunar_tools',
-    version='0.0.1',
+    version='0.0.2',
     packages=find_packages(),
     url='https://github.com/lunarring/lunar_tools',
     description='Lunar Ring auxiliary tools and modules',
