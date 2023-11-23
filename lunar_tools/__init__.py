237ca37f0992d80c30d6733368bcd96c51fb3d7c
Johannes Stelzer <jsdmail@gmail.com>
2023-11-23 11:24:01 +0100

    fixed inits

--- lunar_tools/__init__.py
+++ lunar_tools/__init__.py
@@ -1,4 +1,7 @@
 from .audio import AudioRecorder
-from .audio import SpeechDetector
+from .audio import Speech2Text
+from .audio import Text2Speech
+from .audio import SoundPlayer
+
 from .image_gen import Dalle3ImageGenerator
 from .logprint import LogPrint
