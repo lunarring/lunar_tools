a09ae7839c67f3fa3c38e59ee264eefc168a4b84
Johannes Stelzer <jsdmail@gmail.com>
2023-12-12 17:44:50 +0000

    read env variables handling

--- lunar_tools/utils.py
+++ lunar_tools/utils.py
@@ -1,8 +1,5 @@
-#!/usr/bin/env python3
-# -*- coding: utf-8 -*-
-
+import os
 import platform
-import os 
 
 def get_os_type():
     os_name = platform.system()
@@ -18,5 +15,58 @@ def get_os_type():
         return "Windows"
     else:
         raise ValueError("unsupported OS")
-        
-        
+
+def get_config_path():
+    os_type = get_os_type()
+    if os_type in ["MacOS", "Ubuntu"]:
+        return os.path.expanduser("~/.lunar_tools_env_vars")
+    elif os_type == "Windows":
+        return os.path.join(os.environ['USERPROFILE'], '.lunar_tools_env_vars')
+    else:
+        raise ValueError("Unsupported OS")
+
+def read_all_api_keys_from_lunar_config():
+    config_path = get_config_path()
+    if not os.path.exists(config_path):
+        return {}
+
+    with open(config_path, 'r') as file:
+        lines = file.readlines()
+        return dict(line.strip().split('=') for line in lines if line.strip())
+
+def read_api_key_from_lunar_config(key_name):
+    keys = read_all_api_keys_from_lunar_config()
+    return keys.get(key_name)
+
+def save_api_key_to_lunar_config(key_name, key_value):
+    keys = read_all_api_keys_from_lunar_config()
+    keys[key_name] = key_value
+
+    config_path = get_config_path()
+
+    with open(config_path, 'w') as file:
+        for k, v in keys.items():
+            file.write(f"{k}={v}\n")
+
+def read_api_key(key_name):
+    # First, try to get the API key from the environment variables
+    api_key = os.getenv(key_name)
+
+    if not api_key:
+        # If not found, try to get the key from lunar config
+        api_key = read_api_key_from_lunar_config(key_name)
+
+    if not api_key:
+        # If still not found, prompt the user to input the key
+        print(f"API key for {key_name} not found. Please paste your API key:")
+        api_key = input().strip()
+        save_api_key_to_lunar_config(key_name, api_key)
+
+    return api_key
+
+
+if __name__ == "__main__":
+    # Example usage
+    save_api_key_to_lunar_config('OPENAI_API_KEY', "bba")
+    # # Reading a specific key
+    api_key_value = read_api_key('OPENAI_API_KEY')
