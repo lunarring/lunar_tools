40b91f97ef4a559b4cc44627d887e66f2385c88f
Johannes Stelzer <jsdmail@gmail.com>
2023-12-20 10:41:18 +0100

    zmq image sending

--- lunar_tools/zmq_comms.py
+++ lunar_tools/zmq_comms.py
@@ -4,6 +4,8 @@
 import zmq
 import json
 import threading
+import cv2
+import numpy as np
 from queue import Queue
 from lunar_tools.logprint import LogPrint
 
@@ -14,31 +16,37 @@ class ZMQReceiver:
         self.socket = self.context.socket(zmq.REP)
         self.socket.bind(self.address)
         self.messages = Queue()
+        self.last_image = None
         self.thread = threading.Thread(target=self.run, daemon=True)
         self.logger = logger if logger else LogPrint()
-        self.running = False  # Added to track the running state
+        self.running = False
         if start:
             self.start()
 
     def run(self):
-        self.running = True  # Mark the receiver as running
-        while self.running:  # Check the running state
+        self.running = True
+        while self.running:
             try:
-                message = self.socket.recv(zmq.NOBLOCK)  # Non-blocking receive
-                json_data = json.loads(message.decode('utf-8'))
-                self.messages.put(json_data)
-                self.socket.send(b"Received")
+                message = self.socket.recv(zmq.NOBLOCK)
+                if message.startswith(b'img:'):
+                    nparr = np.frombuffer(message[4:], np.uint8)
+                    self.last_image = cv2.imdecode(nparr, cv2.IMREAD_COLOR)
+                    self.socket.send(b"Image Received")
+                else:
+                    json_data = json.loads(message.decode('utf-8'))
+                    self.messages.put(json_data)
+                    self.socket.send(b"Received")
             except zmq.Again:
-                continue  # Ignore if no message is received
+                continue
 
     def start(self):
         self.thread.start()
 
     def stop(self):
-        self.running = False  # Set running to False to stop the loop
-        self.thread.join()  # Wait for the thread to finish
-        self.socket.close()  # Close the socket
-        self.context.term()  # Terminate the ZMQ context
+        self.running = False
+        self.thread.join()
+        self.socket.close()
+        self.context.term()
 
     def get_messages(self, remove=True):
         messages = []
@@ -46,9 +54,10 @@ class ZMQReceiver:
             messages.append(self.messages.get())
             if remove:
                 self.messages.task_done()
-            
         return messages
 
+    def get_img(self):
+        return self.last_image
 
 
 class ZMQSender:
@@ -57,17 +66,40 @@ class ZMQSender:
         self.context = zmq.Context()
         self.socket = self.context.socket(zmq.REQ)
         self.socket.connect(self.address)
-        self.socket.setsockopt(zmq.RCVTIMEO, timeout * 1000)  # milliseconds
-        self.socket.setsockopt(zmq.SNDTIMEO, timeout * 1000)  # milliseconds
+        self.socket.setsockopt(zmq.RCVTIMEO, timeout * 1000)
+        self.socket.setsockopt(zmq.SNDTIMEO, timeout * 1000)
         self.logger = logger if logger else LogPrint()
+
+        # Default encoding properties
+        self.format = '.jpg'  # Image format
+        self.encode_params = [cv2.IMWRITE_JPEG_QUALITY, 95]  # Encoding parameters
+
+    def configure_image_encoding(self, format='.jpg', **params):
+        self.format = format
+        self.encode_params = []
+        for key, value in params.items():
+            if key == 'jpeg_quality':
+                self.encode_params.extend([cv2.IMWRITE_JPEG_QUALITY, value])
+            elif key == 'png_compression':
+                self.encode_params.extend([cv2.IMWRITE_PNG_COMPRESSION, value])
+            elif key == 'webp_quality':
+                self.encode_params.extend([cv2.IMWRITE_WEBP_QUALITY, value])
 
     def send_json(self, data):
         try:
             json_data = json.dumps(data).encode('utf-8')
             self.socket.send(json_data)
             message = self.socket.recv()
-            message = message.decode('utf-8')
-            return message
+            return message.decode('utf-8')
+        except zmq.Again:
+            return "Timeout"
+
+    def send_img(self, img):
+        _, buffer = cv2.imencode(self.format, img, self.encode_params)
+        try:
+            self.socket.send(b'img:' + buffer.tobytes())
+            message = self.socket.recv()
+            return message.decode('utf-8')
         except zmq.Again:
             return "Timeout"
 
@@ -75,19 +107,29 @@ class ZMQSender:
 
 if __name__ == "__main__":
     # Example usage
-    # First we launch a receiver
     receiver = ZMQReceiver(ip_receiver='127.0.0.1', port_receiver=5556)
+    sender = ZMQSender(ip_receiver='127.0.0.1', port_receiver=5556)
 
-    # And we launch a sender
-    sender = ZMQSender(ip_receiver='127.0.0.1', port=5556)
+    # Send JSON
     reply = sender.send_json({"message": "Hello, Server!", 'bobo': 'huhu'})
     print(f"Received reply: {reply}")
-    
-    # On the receiver, we can get the message
+
+
+
+    # Receive JSON
     msgs = receiver.get_messages()
-    for msg in msgs: # iterating over all messages that were received
-        for field, payload in msg.items(): # iterating over all fields
+    for msg in msgs:
+        for field, payload in msg.items():
             print(f"Field: {field}, Payload: {payload}")
 
-    
-
+    # Receive Image
+    import matplotlib.pyplot as plt
+    for i in range(10):
+        # Send Image
+        test_image = np.random.randint(0, 256, (800, 800, 3), dtype=np.uint8)
+        img_reply = sender.send_img(test_image)
+        print(f"Received image reply: {img_reply}")
+        received_image = receiver.get_img()
+        plt.imshow(received_image)
+        plt.show()
+        
