52ae08f40243bfaefca2d7a5deec484965f1602e
Johannes Stelzer <jsdmail@gmail.com>
2024-02-19 15:57:28 +0000

    skipping rich library for coloring

--- lunar_tools/logprint.py
+++ lunar_tools/logprint.py
@@ -1,9 +1,20 @@
 import os
 import logging
 from datetime import datetime
-from rich.console import Console
-from rich.logging import RichHandler
 import sys
+
+# ANSI escape sequences for colors
+COLORS = {
+    "black": "\u001b[30;1m",
+    "red": "\u001b[31;1m",
+    "green": "\u001b[32;1m",
+    "yellow": "\u001b[33;1m",
+    "blue": "\u001b[34;1m",
+    "magenta": "\u001b[35;1m",
+    "cyan": "\u001b[36;1m",
+    "white": "\u001b[37;1m",
+    "reset": "\u001b[0m"
+}
 
 def dynamic_print(message):
     """
@@ -11,7 +22,6 @@ def dynamic_print(message):
     """
     sys.stdout.write('\r' + message)
     sys.stdout.flush()
-
 
 class LogPrint:
     def __init__(self, filename=None):
@@ -23,14 +33,8 @@ class LogPrint:
             
         self.filename = filename
 
-        self.console = Console()
         self.logger = logging.getLogger(__name__)
         self.logger.setLevel(logging.INFO)
-
-        # Rich console handler for colorful output
-        rich_handler = RichHandler(rich_tracebacks=True, console=self.console)
-        rich_handler.setFormatter(logging.Formatter('%(message)s'))
-        self.logger.addHandler(rich_handler)
 
         # File handler without color formatting
         file_handler = logging.FileHandler(self.filename, mode='w')
@@ -38,32 +42,21 @@ class LogPrint:
         self.logger.addHandler(file_handler)
 
     def print(self, message, color=None):
-        # Printing with color using Rich
-        if color:
-            self.console.print(message, style=f"bold {color}")
+        # Printing with color using ANSI escape sequences
+        if color and color in COLORS:
+            print(COLORS[color] + message + COLORS["reset"])
         else:
-            self.console.print(message)
-        # Temporarily disable console handlers, log the message, then re-enable
-        console_handlers = [h for h in self.logger.handlers if isinstance(h, RichHandler)]
-        for h in console_handlers:
-            self.logger.removeHandler(h)
-
+            print(message)
         self.logger.info(message)
-
-        for h in console_handlers:
-            self.logger.addHandler(h)
 
 if __name__ == "__main__":
     # Example usage
-    # logger = LogPrint()  # No filename provided, will use default current_dir/logs/%y%m%d_%H%M
-    # logger.print("white")
-    # logger.print("red", "red")
-    # logger.print("green", "green")
+    logger = LogPrint()  # No filename provided, will use default current_dir/logs/%y%m%d_%H%M
+    logger.print("This is a white message")
+    logger.print("This is a red message", "red")
+    logger.print("This is a green message", "blue")
 
-    import numpy as np
-    while True:
-        str = f"{np.random.rand():.2f} fps"
-        dynamic_print(str)
-    
-
-
+    # import numpy as np
+    # while True:
+    #     str = f"{np.random.rand():.2f} fps"
+    #     dynamic_print(str)
