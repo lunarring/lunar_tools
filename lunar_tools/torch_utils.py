35a58007cc55a39398c5c796864a2cc5da820f50
lugo <jsdmail@gmail.com>
2024-06-20 16:39:01 +0100

    scaling

--- lunar_tools/torch_utils.py
+++ lunar_tools/torch_utils.py
@@ -3,68 +3,26 @@
 import torch
 import torch.nn as nn
 import torch.nn.functional as F
-from kornia.filters.kernels import get_binary_kernel2d
-from typing import List, Tuple
+from typing import List, Tuple, Optional
+import numpy as np
 
-class MedianBlur(nn.Module):
-    r"""Blurs an image using the median filter.
-
-    Args:
-        kernel_size (Tuple[int, int]): the blurring kernel size.
-
-    Returns:
-        torch.Tensor: the blurred input tensor.
-
-    Shape:
-        - Input: :math:`(B, C, H, W)`
-        - Output: :math:`(B, C, H, W)`
-
-    Example:
-        >>> input = torch.rand(2, 4, 5, 7)
-        >>> blur = MedianBlur((3, 3))
-        >>> output = blur(input)
+def get_binary_kernel2d(
+    window_size: tuple[int, int] | int, *, device: Optional[torch.device] = None, dtype: torch.dtype = torch.float32
+) -> torch.Tensor:
+    """Create a binary kernel to extract the patches.
+    If the window size is HxW will create a (H*W)x1xHxW kernel.
     """
+    if isinstance(window_size, int):
+        ky = kx = window_size
+    else:
+        ky, kx = window_size
+        
+    window_range = kx * ky
+    kernel = torch.zeros((window_range, window_range), device=device, dtype=dtype)
+    idx = torch.arange(window_range, device=device)
+    kernel[idx, idx] += 1.0
+    return kernel.view(window_range, 1, ky, kx)
 
-    def __init__(self, kernel_size: Tuple[int, int]) -> None:
-        super(MedianBlur, self).__init__()
-        self.kernel_size: Tuple[int, int] = kernel_size
-        self.padding: Tuple[int, int] = self._compute_zero_padding(kernel_size)
-        self.kernel: torch.Tensor = self._get_binary_kernel2d(kernel_size)
-
-    @staticmethod
-    def _compute_zero_padding(kernel_size: Tuple[int, int]) -> Tuple[int, int]:
-        r"""Utility function that computes zero padding tuple."""
-        computed: List[int] = [(k - 1) // 2 for k in kernel_size]
-        return computed[0], computed[1]
-
-    @staticmethod
-    def _get_binary_kernel2d(kernel_size: Tuple[int, int]) -> torch.Tensor:
-        """Create a binary kernel for 2D convolution."""
-        kernel = torch.ones((1, 1, *kernel_size))
-        return kernel
-
-    def forward(self, input: torch.Tensor) -> torch.Tensor:
-        if not isinstance(input, torch.Tensor):
-            raise TypeError("Input type is not a torch.Tensor. Got {}"
-                            .format(type(input)))
-
-        if not len(input.shape) == 4:
-            raise ValueError("Invalid input shape, we expect BxCxHxW. Got: {}"
-                             .format(input.shape))
-
-        b, c, h, w = input.shape
-
-        # map the local window to single vector
-        features: torch.Tensor = F.conv2d(
-            input.reshape(b * c, 1, h, w), self.kernel.to(input), padding=self.padding, stride=1)
-        features = features.view(b, c, -1, h, w)  # BxCx(K_h * K_w)xHxW
-
-        # compute the median along the feature axis
-        median: torch.Tensor = torch.median(features, dim=2)[0]
-
-        return median
-    
-    
 class GaussianBlur(nn.Module):
     r"""Blurs an image using a Gaussian filter.
 
@@ -129,9 +87,66 @@ class GaussianBlur(nn.Module):
 
         return features    
     
+class MedianBlur(nn.Module):
+    r"""Blurs an image using the median filter.
+
+    Args:
+        kernel_size (Tuple[int, int]): the blurring kernel size.
+
+    Returns:
+        torch.Tensor: the blurred input tensor.
+
+    Shape:
+        - Input: :math:`(B, C, H, W)`
+        - Output: :math:`(B, C, H, W)`
+
+    Example:
+        >>> input = torch.rand(2, 4, 5, 7)
+        >>> blur = MedianBlur((3, 3))
+        >>> output = blur(input)
+    """
+
+    def __init__(self, kernel_size: Tuple[int, int]) -> None:
+        super(MedianBlur, self).__init__()
+        self.kernel_size: Tuple[int, int] = kernel_size
+        self.padding: Tuple[int, int] = self._compute_zero_padding(kernel_size)
+        self.kernel: torch.Tensor = get_binary_kernel2d(kernel_size)
+
+    @staticmethod
+    def _compute_zero_padding(kernel_size: Tuple[int, int]) -> Tuple[int, int]:
+        r"""Utility function that computes zero padding tuple."""
+        computed: List[int] = [(k - 1) // 2 for k in kernel_size]
+        return computed[0], computed[1]
+
+
+    def forward(self, input: torch.Tensor) -> torch.Tensor:
+        if not isinstance(input, torch.Tensor):
+            raise TypeError("Input type is not a torch.Tensor. Got {}"
+                            .format(type(input)))
+
+        if not len(input.shape) == 4:
+            raise ValueError("Invalid input shape, we expect BxCxHxW. Got: {}"
+                             .format(input.shape))
+
+        b, c, h, w = input.shape
+
+        # map the local window to single vector
+        features: torch.Tensor = F.conv2d(
+            input.reshape(b * c, 1, h, w), self.kernel.to(input), padding=self.padding, stride=1)
+        features = features.view(b, c, -1, h, w)  # BxCx(K_h * K_w)xHxW
+
+        # compute the median along the feature axis
+        median: torch.Tensor = torch.median(features, dim=2)[0]
+
+        return median
+
+
+    
     
 if __name__ == "__main__":
-   tx = torch.rand(2, 4, 5, 7)
-   blur = GaussianBlur((3, 3), 3)
-   output = blur(tx)
+    torch.manual_seed(0)
+    tx = torch.rand(1, 1, 5, 7)
+    
+    blur = GaussianBlur((3, 3), 3)
+    output = blur(tx)
     
