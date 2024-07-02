f99f5e9fddcee2e0d8cb1be93071838e22325a5a
lugo <jsdmail@gmail.com>
2024-07-02 11:38:06 +0100

    fixed osc test error that can appear on ubuntu

--- lunar_tools/tests/test_osc.py
+++ lunar_tools/tests/test_osc.py
@@ -9,7 +9,6 @@ sys.path.append(os.path.join(os.getcwd(), 'lunar_tools'))
 from comms import OSCSender, OSCReceiver
 import numpy as np
 
-
 class TestOSCReceiverClient(unittest.TestCase):
     def setUp(self):
         self.sender = OSCSender('127.0.0.1')
@@ -17,26 +16,30 @@ class TestOSCReceiverClient(unittest.TestCase):
         
     def tearDown(self):
         self.receiver.stop()
-
         
     def test_received_short(self):
+        time.sleep(0.1)
         self.sender.send_message("/env1", 42)
         time.sleep(0.1)
         data = self.receiver.get_all_values("/env1")
-        self.assertEqual(len(data), 1)
-        self.assertEqual(data[0], 42)
-        
+        if data is not None:
+            self.assertEqual(len(data), 1)
+            self.assertEqual(data[0], 42)
+        else:
+            self.fail("No data received from OSC receiver.")
         
     def test_received_long(self):
+        time.sleep(0.1)
         for i in range(10):
             self.sender.send_message("/env1", i)
             time.sleep(0.1)
         data = self.receiver.get_all_values("/env1")
-        self.assertEqual(len(data), 10)
-        for i in range(10):
-            self.assertEqual(data[i], i)
-
-        
+        if data is not None:
+            self.assertEqual(len(data), 10)
+            for i in range(10):
+                self.assertEqual(data[i], i)
+        else:
+            self.fail("No data received from OSC receiver.")
 
 
 
